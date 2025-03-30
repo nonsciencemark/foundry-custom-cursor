dc75ebaac569ad6d31d2de2746f2ee2ddbb1a00f
Mark Holmes <markjamestheholmes@gmail.com>
2025-03-30 11:08:58 +0200

    comments in test.js

---
+++ scripts/lib/lib.js
@@ -0,0 +1,10 @@
+// https://www.freecodecamp.org/news/how-to-make-a-custom-mouse-cursor-with-css-and-javascript/
+
+// js
+// go inside media/sources.json 
+// fill the selection menu from ids in "sources.json"
+// receive default or user input 
+// get url that matches default or user seleciton
+
+// css
+// apply the cursor url to the cursor
--- scripts/test.js
+++ scripts/test.js
@@ -1,17 +1,24 @@
-// packages
-var fs = require('fs');
-
-// test start line
+// test start line =========================================================================
 var msg = 'Hello World';
-console.log(msg);
+// console.log(msg);
 
-// list files in media folder
+// list files in media folder ==============================================================
+var fs = require('fs');
 var files = fs.readdirSync('media');
-console.log(files);
+// console.log(files);
 
-// print path
-console.log('media/' + files)
+// print path ==============================================================================
+// console.log('media/' + files);
 
-// reading
-// var json = require('./media/sources.json'); //(with path)
-// console.log(json)
+// select an option
+var selected_id = 'skyrimcursor';
+// console.log(selected_id);
+
+// reading in the json =====================================================================
+var cursors = require('../media/sources.json').cursors; //(with path)
+
+// cursors.forEach(i => {
+//     console.log(i.id)
+// });
+
+console.log('Selected: ' + cursors.find(i => i.id == selected_id).title)
